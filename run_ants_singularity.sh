abbc102a11fd14b70d99a2e31800495296606e1b
Dan Fitch <dgfitch@gmail.com>
2022-04-14 09:50:50 -0500

    Tweak script

--- run_ants_singularity.sh
+++ run_ants_singularity.sh
@@ -4,14 +4,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
+export SINGULARITYENV_ANTSPATH="/opt/ants/bin" 
 export SINGULARITYENV_PREPEND_PATH="/opt/ants/bin"
 export SINGULARITY_BIND="/study/xyz:/study/xyz"
 
 singularity exec ants.sif \
+  --pwd $DIR \
   antsRegistration \
-  -d 3 -f ../path-$1/anat/sub-$1_T1w_biascorr_brain.nii.gz \
-  -m ../path-$1/func/sub-$1_task-xyz_run1_bold_mod_bet.nii.gz \
-  -o antstest
+  $1 to get passed parameter, and set VARIOUS PARAMETERS HERE
 
 ## If that doesn't work, you can try execing a shell script inside the container like this: 
 #singularity exec ants.sif \
